b8b22c0454ea03e3e968732390aa350b5e283488
daosyn <brandon@daosyn.info>
2024-05-24 16:17:04 -0500

    add grid, confine user movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,32 +1,38 @@
-extends Area2D
+extends CharacterBody2D
 
-signal hit()
+const inputs = {
+	"move_right": Vector2.RIGHT,
+	"move_left": Vector2.LEFT,
+	"move_down": Vector2.DOWN,
+	"move_up": Vector2.UP
+}
 
-@export var speed = 400
+var grid_size = 32
 var screen_size
+var raycast
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	raycast = $RayCast2D
 	$AnimatedSprite2D.play()
 
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+func _unhandled_input(event):
+	for action in inputs.keys():
+		if event.is_action_pressed(action):
+			move(action)
+	
+	#if velocity.x != 0:
+		#$AnimatedSprite2D.flip_v = false
+		#$AnimatedSprite2D.rotation_degrees = 90 if velocity.x > 0 else -90
+	#elif velocity.y != 0:
+		#$AnimatedSprite2D.rotation_degrees = false
+		#$AnimatedSprite2D.flip_v = velocity.y > 0
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	if velocity.x != 0:
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.rotation_degrees = 90 if velocity.x > 0 else -90
-	elif velocity.y != 0:
-		$AnimatedSprite2D.rotation_degrees = false
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+func move(action):
+	var destination = inputs[action] * grid_size
+	#raycast.cast_to = destination
+	#raycast.force_raycast_update()
+	#if not raycast.is_colliding():
+	position += destination
+	position.x = clamp(position.x, 272, 368)
+	position.y = clamp(position.y, 176, 272)
